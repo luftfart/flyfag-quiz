ea0f7a390d4f79ef15af78c3ac620a6c3c8a0fb3
luftfart <140519177+luftfart@users.noreply.github.com>
2024-03-07 11:34:11 +0000

    google search console authorization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
     import Navbar from "$lib/components/Navbar.svelte";
 
 </script>
-
+<svelte:head>
+    <meta name="google-site-verification" content="2-LoIjwQDM5kwhPkjjflcHP9_z6bYmdPeAUPvaNqycg" />
+</svelte:head>
 
 <Navbar></Navbar>
 <slot />
