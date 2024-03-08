772e81e2a20274221108070d475759c57de5eb8b
luftfart <140519177+luftfart@users.noreply.github.com>
2024-03-08 07:29:14 +0000

    adding clarity for usage improvement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,13 @@
 </script>
 <svelte:head>
     <meta name="google-site-verification" content="u9OVdd8C6Nbe6pXvHpbQtctAl4Mmwp93WucGwY9yEgE" />
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "ldcogmj1ce");
+    </script>
 </svelte:head>
 
 <Navbar></Navbar>
